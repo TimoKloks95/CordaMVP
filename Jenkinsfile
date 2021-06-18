af70dac2643cc7e8ca159ad419e57c952c84b757
Timo95 <timokloks@hotmail.com>
2021-06-18 12:15:30 +0200

    First commit

---
+++ Jenkinsfile
@@ -0,0 +1,10 @@
+pipeline {
+    agent any
+    stages {
+        stage('SonarCube') {
+            steps {
+                sh 'runsonar.bat'
+            }
+        }
+    }
+}
---
+++ runsonar.bat
@@ -0,0 +1,3 @@
+::Execute the sonarqube environment
+
+gradlew.bat sonarqube -Dsonar.projectKey=cordamvp -Dsonar.host.url=http://localhost:9000 -Dsonar.login=041be7a281c9d58ba540b981d082710d2774ef7a
