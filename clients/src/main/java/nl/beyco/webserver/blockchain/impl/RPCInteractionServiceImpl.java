7f07665fdbf4c676cf6f789de5f1a3c37342f90a
Timo95 <timokloks@hotmail.com>
2021-07-13 15:04:51 +0200

    Het tracked keyword weggehaald.

--- clients/src/main/java/nl/beyco/webserver/blockchain/impl/RPCInteractionServiceImpl.java
+++ clients/src/main/java/nl/beyco/webserver/blockchain/impl/RPCInteractionServiceImpl.java
@@ -28,7 +28,7 @@ public class RPCInteractionServiceImpl implements RPCInteractionService {
         CordaRPCOps proxy = rpcConnectionService.getProxy();
         log.info("Attempting to call the saveContract flow.");
         try {
-            return proxy.startTrackedFlowDynamic(SaveContractFlow.class, issuerId, contractJson).getReturnValue().get();
+            return proxy.startFlowDynamic(SaveContractFlow.class, issuerId, contractJson).getReturnValue().get();
         } catch (InterruptedException | ExecutionException e) {
             log.error("Something went wrong while calling the saveContract flow.", e);
             throw new BeycoFlowException("Something went wrong while calling the saveContract flow.", e);
@@ -40,7 +40,7 @@ public class RPCInteractionServiceImpl implements RPCInteractionService {
         CordaRPCOps proxy = rpcConnectionService.getProxy();
         log.info("Attempting to call the getContract flow to get contract with ID: "+contractId+" from the blockchain.");
         try {
-            return proxy.startTrackedFlowDynamic(GetContractFlow.class, issuerId, contractId).getReturnValue().get();
+            return proxy.startFlowDynamic(GetContractFlow.class, issuerId, contractId).getReturnValue().get();
         } catch (InterruptedException | ExecutionException e) {
             log.error("Something went wrong while calling the getContract flow to get contract with ID: "+contractId+" from the blockchain.", e);
             throw new BeycoFlowException("Something went wrong while calling the getContract flow to get contract with ID: "+contractId+" from the blockchain.", e);
@@ -52,7 +52,7 @@ public class RPCInteractionServiceImpl implements RPCInteractionService {
         CordaRPCOps proxy = rpcConnectionService.getProxy();
         log.info("Attempting to call the addAddendum flow.");
         try {
-            return proxy.startTrackedFlowDynamic(AddAddendumFlow.class, issuerId, contractId, addendumJson).getReturnValue().get();
+            return proxy.startFlowDynamic(AddAddendumFlow.class, issuerId, contractId, addendumJson).getReturnValue().get();
         } catch (InterruptedException | ExecutionException e) {
             log.error("Something went wrong while calling the addAddendum flow.", e);
             throw new BeycoFlowException("Something went wrong while calling the addAddendum flow.", e);
