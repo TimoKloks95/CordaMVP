74e23a7e420eacbed5badd424134d4d1f686e190
Timo95 <timokloks@hotmail.com>
2021-07-20 11:23:37 +0200

    Overbodige modifiers weggehaald.

--- clients/src/main/java/nl/beyco/webserver/helpers/BeycoSerializer.java
+++ clients/src/main/java/nl/beyco/webserver/helpers/BeycoSerializer.java
@@ -1,6 +1,6 @@
 package nl.beyco.webserver.helpers;
 
 public interface BeycoSerializer {
-    public <T> Object toObject(String jsonString, Class<T> valueType);
-    public String toJson(Object obj);
+    <T> Object toObject(String jsonString, Class<T> valueType);
+    String toJson(Object obj);
 }
